2f3bc1eac8f97c8cdf74b0274333ac4e860ce744
Simon Balean <simonbalean@gmail.com>
2018-11-14 06:21:32 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,5 +7,12 @@ def array_with_two_elements
 end
 
 def first_element(indexing)
-  Array.shift
+  indexing(0)  
+end
+
+def third_element(indexing)
+  indexing(2)
+end
+
+def last_element(indexing)
 end
